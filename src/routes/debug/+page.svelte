0ca2ed62643ad30ed5cbbdbcce301eba993278b8
Nicholas Wolff <nwolff@gmail.com>
2024-04-01 10:57:51 +0200

    remove tfvis from /debug

--- src/routes/debug/+page.svelte
+++ src/routes/debug/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { networkStore } from '../../stores';
-	import * as tf from '@tensorflow/tfjs';
 	import DistributionChart from '$lib/components/DistributionChart.svelte';
 	import DrawBox from '$lib/components/DrawBox.svelte';
 	import ConfusionMatrix from '$lib/components/ConfusionMatrix.svelte';
 	import { Grid, Button, Loader, Title, Text, Divider, Stack } from '@svelteuidev/core';
 
-	let tfvis;
+	import * as tf from '@tensorflow/tfjs';
 
 	const labels = ['A', 'B', 'C', 'D'];
 	const percentages = [0, 0.2, 0.5, 0.3];
@@ -21,20 +20,8 @@
 	let tfStats: string;
 
 	onMount(async () => {
-		tfvis = await import('@tensorflow/tfjs-vis');
-		showModelSummary();
 		showStats();
 	});
-
-	async function showModelSummary() {
-		const tfModel: tf.Sequential = $networkStore.tfModel;
-		const summaryContainer = { name: 'Résumé du modèle', tab: 'Inspection' };
-		tfvis.show.modelSummary(summaryContainer, tfModel);
-		for (const [index, layer] of tfModel.layers.entries()) {
-			const layerContainer = { name: 'Couche ' + index, tab: 'Inspection' };
-			tfvis.show.layer(layerContainer, layer);
-		}
-	}
 
 	async function showStats() {
 		tfStats = JSON.stringify(tf.memory(), null, ' ');
