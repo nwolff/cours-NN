694b181f64e994b3e46b5f15775e9869f1769ef5
Nicholas Wolff <nwolff@gmail.com>
2026-04-14 21:16:20 +0200

    remove square stores

--- src/stores.ts
+++ src/stores.ts
@@ -1,50 +1,51 @@
-import { asyncWritable, writable } from '@square/svelte-store';
+import { writable } from 'svelte/store';
 import { newAllDigitsNetwork } from '$lib/networks/AllDigitsNetwork';
 import { newZeroOneNetwork } from '$lib/networks/ZeroOneNetwork';
 import { newFashionNetwork } from '$lib/networks/FashionNetwork';
 import { newTemperatureControlNetwork } from '$lib/networks/TemperatureControlNetwork';
 import type { Network } from '$lib/Network';
 
-export const temperatureControlNetworkStore = asyncWritable<never[], Network>(
-	[],
-	async () => {
-		return newTemperatureControlNetwork();
-	},
-	undefined,
-	{ reloadable: true }
+function asyncReloadable<T>(loader: () => Promise<T>) {
+	const { subscribe, set } = writable<T>(undefined as unknown as T);
+	const load = async () => {
+		const value = await loader();
+		set(value);
+	};
+	return { subscribe, load, reload: load };
+}
+
+function persistable<T>(initialValue: T) {
+	const { subscribe, set, update } = writable(initialValue);
+	let current = initialValue;
+	subscribe((v) => (current = v));
+	return {
+		subscribe,
+		set,
+		update,
+		load: () => Promise.resolve(current)
+	};
+}
+
+export const temperatureControlNetworkStore = asyncReloadable(async () =>
+	newTemperatureControlNetwork()
 );
 
-export const zeroOnenetworkStore = asyncWritable<never[], Network>(
-	[],
-	async () => {
-		const network = newZeroOneNetwork();
-		await network.dataSource.loadData();
-		return network;
-	},
-	undefined,
-	{ reloadable: true }
-);
+export const zeroOnenetworkStore = asyncReloadable(async () => {
+	const network = newZeroOneNetwork();
+	await network.dataSource.loadData();
+	return network;
+});
 
-export const allDigitsNetworkStore = asyncWritable<never[], Network>(
-	[],
-	async () => {
-		const network = newAllDigitsNetwork();
-		await network.dataSource.loadData();
-		return network;
-	},
-	undefined,
-	{ reloadable: true }
-);
+export const allDigitsNetworkStore = asyncReloadable(async () => {
+	const network = newAllDigitsNetwork();
+	await network.dataSource.loadData();
+	return network;
+});
 
-export const fashionNetworkStore = asyncWritable<never[], Network>(
-	[],
-	async () => {
-		const network = newFashionNetwork();
-		await network.dataSource.loadData();
-		return network;
-	},
-	undefined,
-	{ reloadable: true }
-);
+export const fashionNetworkStore = asyncReloadable(async () => {
+	const network = newFashionNetwork();
+	await network.dataSource.loadData();
+	return network;
+});
 
-export const learningRateStore = writable<number>(0.2);
+export const learningRateStore = persistable(0.2);
