e296438e754aaa833751f3881471f55f20925626
Nicholas Wolff <nwolff@gmail.com>
2026-04-15 12:17:19 +0200

    Improve about page: links and timezone
    - Repo and commit hash are now clickable links
    - Commit displays as a 7-char short SHA (full SHA still used in the href)
    - Timestamp now produced in Europe/Zurich timezone with a readable format
      (e.g. 2026-04-15 14:23 CEST)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,33 @@
+<script>
+	const repoUrl = '[GITHUB_SERVER_URL]/[GITHUB_REPOSITORY]';
+	const repoName = '[GITHUB_REPOSITORY]';
+	const commitUrl = '[GITHUB_SERVER_URL]/[GITHUB_REPOSITORY]/commit/[GITHUB_SHA]';
+	const shortSha = '[SHORT_SHA]';
+	const refName = '[GITHUB_REF_NAME]';
+	const timestamp = '[TIMESTAMP]';
+
+	const deployed = repoUrl.startsWith('http');
+</script>
+
 <dl>
 	<dt class="font-semibold">repo</dt>
-	<dd class="ml-3">[GITHUB_SERVER_URL]/[GITHUB_REPOSITORY]</dd>
+	<dd class="ml-3">
+		{#if deployed}
+			<a class="link" href={repoUrl} target="_blank" rel="noreferrer">{repoName}</a>
+		{:else}
+			{repoName}
+		{/if}
+	</dd>
 	<dt class="font-semibold">branch</dt>
-	<dd class="ml-3">[GITHUB_REF_NAME]</dd>
-	<dt class="font-semibold">hash</dt>
-	<dd class="ml-3">[GITHUB_SHA]</dd>
-	<dt class="font-semibold">build-timestamp</dt>
-	<dd class="ml-3">[TIMESTAMP]</dd>
+	<dd class="ml-3">{refName}</dd>
+	<dt class="font-semibold">commit</dt>
+	<dd class="ml-3">
+		{#if deployed}
+			<a class="link" href={commitUrl} target="_blank" rel="noreferrer">{shortSha}</a>
+		{:else}
+			{shortSha}
+		{/if}
+	</dd>
+	<dt class="font-semibold">built</dt>
+	<dd class="ml-3">{timestamp}</dd>
 </dl>
